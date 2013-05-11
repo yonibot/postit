3d08ec741d4d32979159886639b54a76d6135c07
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2013-05-11 11:01:32 +0300

    Created User, Post, and Category models

---
+++ db/migrate/20130510171838_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def up
+  	create_table :posts do |t|
+  		t.string :url, :title
+  		t.text :description
+  		t.timestamps
+  	end
+  end
+
+  def down
+  	drop_table :posts
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320000836) do
+ActiveRecord::Schema.define(:version => 20130510193156) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "url"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
